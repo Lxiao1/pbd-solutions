738882038648d74372e262bd3156b5cbe91b23ac
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-02-14 10:44:41 -0500

    Add files via upload

---
+++ 05/CommentSlashes.java
@@ -0,0 +1,13 @@
+public class CommentSlashes
+{
+	public static void main( String[] args)
+	{
+		//java is always ignoring me :(
+		//I wish it would listen to me
+
+		System.out.println( "Ha I get to appear on the screen." );
+		//Why can't I be cool like System.out.println :(
+		//System.out.println(" Hello? Is anyone there?" )
+		System.out.println( ">:)" );
+	}
+}
