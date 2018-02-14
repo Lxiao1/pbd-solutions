3979c0c6dab0479c6de6bee5b3b62eb7d9a3f893
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-02-14 11:11:01 -0500

    Add files via upload

---
+++ 06/letter.java
@@ -0,0 +1,17 @@
+public class letter
+{
+	public static void main (String[] args)
+	{
+	System.out.println( "+--------------------------------------------+" );
+	System.out.println( "|                                        ####|" );
+	System.out.println( "|                                        ####|" );
+	System.out.println( "|                                        ####|" );
+	System.out.println( "|                                            |" );
+	System.out.println( "|                                            |" );
+	System.out.println( "|                         Bill Gates         |" );
+	System.out.println( "|                         1 Microsoft Way    |" );
+	System.out.println( "|                         Redmond, WA 89104  |" );
+	System.out.println( "|                                            |" );
+	System.out.println( "+--------------------------------------------+" );
+	}
+}
