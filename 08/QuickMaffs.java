b2152076df676085c90dae2dcfb65518b3cdf9bd
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-02-15 11:00:26 -0500

    Add files via upload

---
+++ 08/QuickMaffs.java
@@ -0,0 +1,9 @@
+public class QuickMaffs
+{
+	public static void main(String[] args)
+	{
+	System.out.println( "I will now do math:" );
+	System.out.println( "1 + 1 = " + (1 + 1) );
+	System.out.println( "2 + 2 - 1 = " + (1 + 1 + 1 + 1) );
+	}
+}
