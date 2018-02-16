f35b1bcb553d31543c80645ba52b44565ce6bed8
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-02-16 10:29:47 -0500

    Update GoodFirstProgram.java

--- 04/GoodFirstProgram.java
+++ 04/GoodFirstProgram.java
@@ -1,13 +1,11 @@
-public class GoodFirstProgram
-{
-	public static void main( String[] args )
-	{
-		System.out.println( "Hello World!" ); //prints hello world
-		System.out.println( "Hello again!" ); //prints hello again
-		System.out.println( "I like typing this." ); //more printing
-		System.out.println( "This is fun." ); //more printing
-		System.out.println( "Yay! Printing. "); //more printing
-		System.out.println( "I'd much rather you 'not'." ); //more printing
-		System.out.println( "I \"said\" do not touch this." ); //more printing
+public class GoodFirstProgram{
+	public static void main( String[] args ) {
+		System.out.println("Hello World!"); //prints hello world
+		System.out.println("Hello again!"); //prints hello again
+		System.out.println("I like typing this."); //more printing
+		System.out.println("This is fun."); //more printing
+		System.out.println("Yay! Printing."); //more printing
+		System.out.println("I'd much rather you 'not'."); //more printing
+		System.out.println("I \"said\" do not touch this."); //more printing
 	}
 }
