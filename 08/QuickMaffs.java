757d41624b6bb59f5fcc747451431331d0769026
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-02-16 10:16:23 -0500

    Update QuickMaffs.java

--- 08/QuickMaffs.java
+++ 08/QuickMaffs.java
@@ -1,9 +1,7 @@
-public class QuickMaffs
-{
-	public static void main(String[] args)
-	{
-	System.out.println( "I will now do math:" );
-	System.out.println( "1 + 1 = " + (1 + 1) );
-	System.out.println( "2 + 2 - 1 = " + (1 + 1 + 1 + 1) );
+public class QuickMaffs { 
+	public static void main(String[] args) {
+		System.out.println("I will now do math:"); //comments to the reader "I will now do math"
+		System.out.println("1 + 1 = " + (1.0 + 1.0)); //displays 1 + 1 = and then calculates 1+1 and displays it
+		System.out.println("2 + 2 - 1 = " + (1.0 + 1.0 + 1.0)); //displays 2 + 2 - 1 = and then calculates it
 	}
 }
