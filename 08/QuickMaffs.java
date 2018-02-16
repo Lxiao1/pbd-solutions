5716d659f1443e5d2719927a086a03424787994d
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-02-16 10:20:53 -0500

    Update QuickMaffs.java

--- 08/QuickMaffs.java
+++ 08/QuickMaffs.java
@@ -3,5 +3,8 @@ public class QuickMaffs {
 		System.out.println("I will now do math:"); //comments to the reader "I will now do math"
 		System.out.println("1 + 1 = " + (1.0 + 1.0)); //displays 1 + 1 = and then calculates 1+1 and displays it
 		System.out.println("2 + 2 - 1 = " + (1.0 + 1.0 + 1.0)); //displays 2 + 2 - 1 = and then calculates it
+		System.out.println( "Is 5 greater than -2? " + ( 5 > -2 ) ); //calculates if 5 > -2
+		System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) ); //calculates if 5 >= -2
+		System.out.println( "Is it less or equal? " + ( 5 <= -2 ) ); //calculates if 5 <= -2
 	}
 }
