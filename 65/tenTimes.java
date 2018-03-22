471de71628972fa1351f7b772bd09d2d53944a41
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2018-03-22 10:07:48 -0400

    Add files via upload

---
+++ 65/tenTimes.java
@@ -0,0 +1,7 @@
+public class tenTimes{
+    public static void main(String[] args){
+        for(int x = 1 ; x < 1 ; x = x + 1){
+            System.out.println(x + ". Mr. Gallo is very very cool.");
+        }
+    }
+}
